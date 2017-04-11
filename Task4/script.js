f60037b6f58608ae7fb38024fd4b08bdeef1c973
Yosip92 <yosip92@gmail.com>
2017-04-12 02:42:22 +0300

    fix

--- Task4/script.js
+++ Task4/script.js
@@ -1,11 +1,11 @@
 function myFunc(){
     var txt = ""; 
-var x, myObj;
-// y = document.getElementById('text1').innerHTML;
-myObj = console;    /*input value here*/
+var x, myObj, tre;
+//  myObj = document.getElementById('text1').innerHTML;
+myObj = history;    /*input value here*/
 for (x in myObj) {
     txt += myObj[x];
 }
 document.getElementById("param").innerHTML = txt;
-document.getElementById("type").innerHTML = typeof myObj;
-} 
+document.getElementById("type").innerHTML = "This is " + typeof myObj + '!';
+}
