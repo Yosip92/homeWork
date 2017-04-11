db5542450e6753836b302851b23a49c976cd53bc
Yosip92 <yosip92@gmail.com>
2017-04-11 23:19:39 +0300

    fixed code

--- Task4/script.js
+++ Task4/script.js
@@ -1,7 +1,8 @@
 function myFunc(){
     var txt = ""; 
-var x, y ;
-y = document.getElementById('text1').innerHTML;
+var x, y;
+// y = document.getElementById('text1').innerHTML;
+y = /*input value here*/;
 for (x in y) {
     txt += y[x] + " ";
 }
