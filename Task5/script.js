f0111a7753c9d73182855cfc05ed1e4e9fc49fd7
Yosip92 <yosip92@gmail.com>
2017-04-17 15:52:43 +0300

    done task5

--- Task5/script.js
+++ Task5/script.js
@@ -1,36 +1,26 @@
 function myFunc() {
-    var audi  = {firmName:"Audi" , modelName:"TT" , engDisp:2},
-        bentley = {firmName:"Bentley" , modelName:"Continental" , engDisp:6},
-        ford = {firmName:"Ford" , "modelName":"Mustang" , "engDisp":4.6},
-        daewoo = {firmName:"Daewoo" , modelName:"Lanos" , engDisp:1.5},
-        tesla = {firmName:"Tesla" , modelName:"Model S" , engDisp:0 , electMot:270},
-        auto = {audi, bentley, ford, daewoo, tesla};
-    // var auto = [ ["firmName":"Audi" , "modelName":"TT" , "engDisp":2],["firmName":"Bentley" , "modelName":"Continental" , "engDisp":6],["firmName":"Ford" , "modelName":"Mustang" , "engDisp":4.6],["firmName":"Daewoo" , "modelName":"Lanos" , "engDisp":1.5], ["firmName":"Tesla" , "modelName":"Model S" , "engDisp":0 , "electMot":270] ];
-    var engDisp = x;
-    x = document.getElementById("engine").innerHTML;
-
-    if (x == 2){
-        document.getElementById("result").innerHTML = text;
-        text = ("firmName" + "modelName" + "engDisp");
+    auto = new Array(
+        {firmName:"Audi" , modelName:"TT" , engDisp:2},
+        {firmName:"Bentley" , modelName:"Continental" , engDisp:6},
+        {firmName:"Ford" , modelName:"Mustang" , engDisp:4.6},
+        {firmName:"Daewoo" , modelName:"Lanos" , engDisp:1.5}
+    );
+    var engine = document.getElementById("engine").value, x, asd = engine/1000;
+    if (asd <= auto[3].engDisp){
+        x = 3;
     }
-    // var little = tesla.slice(0,3);
-    // console.log(little);
-    //if (x <= auto.audi["engDisp"]){
-    //     document.getElementById("result").innerHTML = auto.audi["modelName"] + auto.audi["firmName"];
-    // }
-    // else if(x) {
-    //     document.getElementById("result").innerHTML = "null";
-    //}
-    // if (x <= 6){
-    //     document.getElementById("result").innerHTML = auto.bentley["modelName"] + auto.bentley["firmName"];
-    // };
-    
-    // var x = document.getElementById("engine").innerHTML;
-    //if (x <= engDisp) {
-      //   text = auto;
-    //};
-    // console.log(auto.length);
-    // document.getElementById("result").innerHTML =
-    // auto["firmName", "modelName", "electMot"] + x + " litres ";
-    //document.getElementById("result").innerHTML = text;
-}()
+    else if (asd <= auto[0].engDisp){
+        x = 0;
+    }
+    else if (asd <= auto[2].engDisp){
+        x = 2;
+    }
+    else if (asd <= auto[1].engDisp){
+        x = 1;
+    }
+    else {
+        document.getElementById('result').innerHTML = "We have not that car!";
+    }
+
+    document.getElementById('result').innerHTML = "Your car in our shop is: " + auto[x].firmName + " " + auto[x].modelName + " " + auto[x].engDisp + " littres!";
+}
