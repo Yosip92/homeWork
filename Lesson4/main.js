dc2c9623d4c7568624efd1089ff5585d51fc1d63
Dmitro Raday <yosip92@gmail.com>
2017-05-19 21:03:49 +0300

    task4

---
+++ Lesson4/main.js
@@ -0,0 +1,20 @@
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://desolate-dawn-81748.herokuapp.com/api/v1/feedbacks', false);
+
+// console.log(xhr);
+// xhr.send();
+// alert(xhr.responseText.id[63].name);
+ $( document ).ready(function() {
+            var ajaxData;
+            $.ajax({
+              dataType: 'json',
+              url: 'https://desolate-dawn-81748.herokuapp.com/api/v1/feedbacks',
+              success: function(jsondata){
+                ajaxData = "<div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 10%;'>ID</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 40%;'>Name</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 20%;'>Age</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 30%;'>Date</div></div>";
+                jsondata.map((person) => {
+                    ajaxData = ajaxData + "<div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 10%;'>" + person.id + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 40%;'>" + person.name + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 20%;'>" + person.age + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 30%;'>" + person.date + "</div></div>";
+                })
+                $("#tableContent").html("<p> 123 </p>" + ajaxData);
+              }
+            });
+        });
