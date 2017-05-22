c2727316052a923b34b96af818b102951acfbeb7
Dmitro Raday <yosip92@gmail.com>
2017-05-22 21:02:18 +0300

    dont work bootstrap

--- Lesson4/main.js
+++ Lesson4/main.js
@@ -4,8 +4,11 @@
 // console.log(xhr);
 // xhr.send();
 // alert(xhr.responseText.id[63].name);
- $( document ).ready(function() {
-            var ajaxData;
+ $(document).ready(function() {
+            getAjaxData();
+        });
+        function getAjaxData() {
+          var ajaxData;
             $.ajax({
               dataType: 'json',
               url: 'https://desolate-dawn-81748.herokuapp.com/api/v1/feedbacks',
@@ -14,7 +17,22 @@
                 jsondata.map((person) => {
                     ajaxData = ajaxData + "<div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 10%;'>" + person.id + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 40%;'>" + person.name + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 20%;'>" + person.age + "</div><div style='border: 1px solid; box-sizing: border-box; float: left; width: 30%;'>" + person.date + "</div></div>";
                 })
-                $("#tableContent").html("<p> 123 </p>" + ajaxData);
+                $("#tableContent").html(ajaxData);
               }
             });
-        });
+        }
+function postAjaxData() {
+  console.log($('#age').val());
+  $.ajax({
+        dataType: 'json',
+        data: {
+          age: $('#age').val(),
+          name: $('#name').val(),
+          date: $('#date').val(),
+        },
+        method: 'post',
+        url: 'https://desolate-dawn-81748.herokuapp.com/api/v1/feedbacks',
+    });
+    getAjaxData();
+}
+               
